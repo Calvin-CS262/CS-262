6f61a2f75b97aba98460950abcbeea6ac6bbd845
Seeun Oh <so37@gold03.cs.calvin.edu>
2018-10-03 20:55:24 -0400

    Add CreateARide

---
+++ CreateARide/app/src/main/java/so37/cs262/calvin/edu/createaridepage/MainActivity.java
@@ -0,0 +1,16 @@
+package so37.cs262.calvin.edu.createaridepage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
