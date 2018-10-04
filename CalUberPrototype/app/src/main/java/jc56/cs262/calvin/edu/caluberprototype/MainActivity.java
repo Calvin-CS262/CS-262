e4edafeacf545c431720d356ab0a60dd0e2811b9
Valeria Martinez <valeriamartinez@dhcp84-227.calvin.edu>
2018-10-03 21:20:40 -0400

    this is the first profile page -- it's really bad, I'll change it later tonight

--- CalUberPrototype/app/src/main/java/jc56/cs262/calvin/edu/caluberprototype/MainActivity.java
+++ CalUberPrototype/app/src/main/java/jc56/cs262/calvin/edu/caluberprototype/MainActivity.java
@@ -2,6 +2,8 @@ package jc56.cs262.calvin.edu.caluberprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchUserProfile(View view) {
+        Intent intent = new Intent(this,UserProfile.class);
+        startActivity(intent);
+
     }
 }
