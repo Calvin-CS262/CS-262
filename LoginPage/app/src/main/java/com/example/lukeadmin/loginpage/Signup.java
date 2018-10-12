27e1912398f4616517f50ec2c7116700a0c737db
Luke Steffen <lhsteffen527@gmail.com>
2018-10-11 23:20:02 -0400

    Added sign up and connections

--- LoginPage/app/src/main/java/com/example/lukeadmin/loginpage/Signup.java
+++ LoginPage/app/src/main/java/com/example/lukeadmin/loginpage/Signup.java
@@ -5,21 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 
-    public void sign_up_page(View view) {
-        Intent intent = new Intent(this, Signup.class);
-        startActivity(intent);
-    }
-
-    public void go_to_home(View view) {
-        Intent intent = new Intent(this, HomePage.class);
+    public void return_to_login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
