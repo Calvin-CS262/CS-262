1d4c5ba91c5e7d56449a91f83ee2c83ae6466c69
Jae Young Choi(jc56) <choi.jaeyoung@outlook.com>
2018-12-12 03:13:42 -0500

    Help button included

--- app/src/main/java/jc56/cs262/calvin/edu/caluberprototype/HomePage.java
+++ app/src/main/java/jc56/cs262/calvin/edu/caluberprototype/HomePage.java
@@ -1,8 +1,13 @@
 package jc56.cs262.calvin.edu.caluberprototype;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,7 +27,6 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
     }
 
     public void launchJoinRide(View view) {
@@ -50,4 +54,7 @@ public class HomePage extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
+
 }
